83ad475e760d8a40fc21648c0d4be351528a2fc5
ahmedmansour3548 <ahmed.mansour3548@gmail.com>
2024-04-08 10:24:01 -0400

    moar

--- MRInteractiveMural.Server/obj/Debug/net8.0/MRInteractiveMural.Server.AssemblyInfo.cs
+++ MRInteractiveMural.Server/obj/Debug/net8.0/MRInteractiveMural.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MRInteractiveMural.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13cd8415ae50619599b55436bb2b62eee9fb016")]
 [assembly: System.Reflection.AssemblyProductAttribute("MRInteractiveMural.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MRInteractiveMural.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
